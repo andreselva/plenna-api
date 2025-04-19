ecc10d04517e36211207295b83920a57cd5499db
André Saraiva Selva <121300349+andreselva@users.noreply.github.com>
2025-04-19 17:04:51 -0300

    ajustes revenues/expenses

--- src/Expenses/UseCases/UpdateExpense.ts
+++ src/Expenses/UseCases/UpdateExpense.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@nestjs/common";
 import { ExpenseDTO } from "../DTOs/ExpenseDTO";
 import { Expense } from "../Entity/Expense";
 import { ExpensesRepository } from "../ExpensesRepository";
-import { FormatDate } from "src/Shared/Utils/FormatDate";
 
 @Injectable()
 export class UpdateExpense {
@@ -12,7 +11,6 @@ export class UpdateExpense {
 
     async execute(id: string, expense: ExpenseDTO) {
         expense.id = Number(id);
-        expense.invoiceDueDate = FormatDate.formatToYYYYMMDD(expense.invoiceDueDate);
         const entity = Expense.fromDTO(expense);
         return await this.repository.updateExpense(entity)
     }
--- src/Revenues/UseCases/UpdateRevenue.ts
+++ src/Revenues/UseCases/UpdateRevenue.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@nestjs/common";
 import { RevenueDTO } from "../DTOs/RevenueDTO";
 import Revenue from "../Entity/Revenue";
 import RevenuesRepository from "../RevenuesRepository";
-import { FormatDate } from "src/Shared/Utils/FormatDate";
 
 @Injectable()
 export class UpdateRevenue {
@@ -12,7 +11,6 @@ export class UpdateRevenue {
 
     async execute(id: string, revenue: RevenueDTO) {
         revenue.id = Number(id);
-        revenue.invoiceDueDate = FormatDate.formatToYYYYMMDD(revenue.invoiceDueDate);
         const entity = Revenue.fromDTO(revenue);
         return await this.repository.updateRevenue(entity);
     }

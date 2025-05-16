a9557a0a8b13628f6400298d3c6669e1fd44fc2a
André Saraiva Selva <121300349+andreselva@users.noreply.github.com>
2025-05-15 21:23:40 -0300

    ajusta jwtguard

--- src/Auth/JwtAuthGuard.ts
+++ src/Auth/JwtAuthGuard.ts
@@ -1,5 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+    getRequest(context: ExecutionContext) {
+        const request = context.switchToHttp().getRequest();
+
+        const accessToken = request.cookies['access_token'];
+        if (accessToken) {
+            request.headers.authorization = `Bearer ${accessToken}`;
+        }
+
+        return request;
+    }
+}

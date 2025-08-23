bf6ba16ecd17ffe5b494d11284fdf4ee0e3ebf98
André Selva <selvaandre99@gmail.com>
2025-08-22 21:31:36 -0300

    ajustes na paleta de cores do dashboard

--- src/modules/Dashboard/CreditCardStatements/ChartConfig/CreditCardStatementsChartConfig.ts
+++ src/modules/Dashboard/CreditCardStatements/ChartConfig/CreditCardStatementsChartConfig.ts
@@ -1,18 +1,18 @@
 export default class CreditCardStatementsChartConfig {
     static readonly backgroundColor = [
-        'rgba(230, 67, 102, 1)',
-        'rgba(54, 162, 235, 0.7)',
-        'rgba(255, 205, 86, 0.7)',
-        'rgba(75, 192, 192, 0.7)',
-        'rgba(153, 102, 255, 0.7)',
+        'rgba(153, 102, 255, 1)',   
+        'rgba(255, 159, 64, 1)',    
+        'rgba(54, 162, 235, 1)',   
+        'rgba(255, 205, 86, 1)',    
+        'rgba(75, 192, 192, 1)'
     ];
 
     static readonly borderColor = [
-        'rgba(226, 57, 94, 1)',
+        'rgb(153, 102, 255)',
+        'rgb(255, 159, 64)',
         'rgb(54, 162, 235)',
         'rgb(255, 205, 86)',
-        'rgb(75, 192, 192)',
-        'rgb(153, 102, 255)',
+        'rgb(75, 192, 192)'
     ];
 
     static readonly borderWidth = 1;
--- src/modules/Dashboard/ExpensesByCategory/ChartConfig/ExpensesByCategoryChartConfig.ts
+++ src/modules/Dashboard/ExpensesByCategory/ChartConfig/ExpensesByCategoryChartConfig.ts
@@ -1,18 +1,18 @@
 export default class ExpensesByCategoryChartConfig {
     static readonly backgroundColor = [
-        'rgba(255, 99, 132, 0.7)',
-        'rgba(54, 162, 235, 0.7)',
-        'rgba(255, 205, 86, 0.7)',
-        'rgba(75, 192, 192, 0.7)',
-        'rgba(153, 102, 255, 0.7)',
+        'rgba(153, 102, 255, 1)',   
+        'rgba(255, 159, 64, 1)',    
+        'rgba(54, 162, 235, 1)',   
+        'rgba(255, 205, 86, 1)',    
+        'rgba(75, 192, 192, 1)'
     ];
 
     static readonly borderColor = [
-        'rgb(255, 99, 132)',
+        'rgb(153, 102, 255)',
+        'rgb(255, 159, 64)',
         'rgb(54, 162, 235)',
         'rgb(255, 205, 86)',
-        'rgb(75, 192, 192)',
-        'rgb(153, 102, 255)',   
+        'rgb(75, 192, 192)'
     ];
 
     static readonly borderWidth = 1;

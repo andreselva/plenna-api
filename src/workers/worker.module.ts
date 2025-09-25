e6752d0a98afdc0f93b4380e11c63a8a2107082f
André Saraiva Selva <121300349+andreselva@users.noreply.github.com>
2025-09-24 22:24:02 -0300

    fix: disponibilizar contexto de auth nos workers

--- src/workers/worker.module.ts
+++ src/workers/worker.module.ts
@@ -1,33 +1,29 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { WorkerAuthContextService } from './worker-auth-context.service';
 import { AppointmentsModule } from 'src/modules/appointments/appointments.module';
 import { EmailModule } from 'src/modules/email/email.module';
 
 // módulos que os serviços de agendamento usam (DB/Redis)
 import { RedisModule } from 'src/modules/redis/redis.module';
 import { DatabaseModule } from 'src/modules/Config/Database/database.module';
-import { AuthContextService } from 'src/modules/Auth/auth-context.service';
+
 import { AppointmentsWorkerService } from './appointments-worker.service';
 import { EmailWorkerService } from './email-worker.service';
+import { WorkerAuthModule } from './worker-auth.module';
 
 // mapearemos este token para a nossa implementação do worker
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    WorkerAuthModule,
     DatabaseModule,
     RedisModule,
     AppointmentsModule,
     EmailModule,
   ],
   providers: [
-    WorkerAuthContextService,
-    {
-      provide: AuthContextService,
-      useExisting: WorkerAuthContextService,
-    },
     // Serviços responsáveis por processar os jobs das filas
     AppointmentsWorkerService,
     EmailWorkerService,

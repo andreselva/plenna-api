a013f66d30de5081584e6956ac5b498f0eb86558
André Selva <selvaandre99@gmail.com>
2026-02-13 14:48:51 -0300

    ajusta

--- src/modules/client-modules/client-modules.service.ts
+++ src/modules/client-modules/client-modules.service.ts
@@ -29,7 +29,7 @@ export class ClientModulesService {
         const modules = await this.repository.getModulesByUserId();
         const organized = this.organize(modules);
         this.logger.log(`Variável de ambiente: ${process.env.NODE_ENV}`);
-        if (process.env.NODE_ENV === Environment.DEVELOPMENT) {
+        if (process.env.NODE_ENV === Environment.PRODUCTION) {
             await this.saveModulesTree(organized);
         }
         return {

21a4083c4a22f0d3b4df6ee1ffda18b554c72756
André Selva <selvaandre99@gmail.com>
2026-02-13 14:47:10 -0300

    ajustado posição para ver retorno

--- src/modules/client-modules/client-modules.service.ts
+++ src/modules/client-modules/client-modules.service.ts
@@ -20,8 +20,8 @@ export class ClientModulesService {
     async getModules() {
         this.logger.log("Getting modules.");
         const cachedModules = await this.getModulesTreeFromCache();
-        this.logger.log(`Return cachedModules: ${cachedModules}`);
         if (cachedModules) {
+            this.logger.log(`Returned cachedModules: ${{cachedModules}}`);
             return {
                 modules: cachedModules
             };

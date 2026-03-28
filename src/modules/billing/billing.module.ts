d2382b1481843a1df1e6d155524bd6f28b0279bd
André <selvaandre99@gmail.com>
2026-03-27 22:10:51 -0300

    ajusta modulo billing

--- src/modules/billing/billing.module.ts
+++ src/modules/billing/billing.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
-import { BillingService } from './billing.service';
-import { BillingController } from './billing.controller';
 import { GatewaysModule } from './gateways/gateways.module';
 import { BillingRulesModule } from './billing-rules/billing-rules.module';
 import { PaymentMethodsModule } from './payment-methods/payment-methods.module';
 import { ChargesModule } from './charges/charges.module';
 
 @Module({
-  providers: [BillingService],
-  controllers: [BillingController],
+  providers: [],
+  controllers: [],
   imports: [GatewaysModule, BillingRulesModule, PaymentMethodsModule, ChargesModule]
 })
 export class BillingModule {}

6f44632411e5b2d513244fec6f82209f39274d54
André Selva <selvaandre99@gmail.com>
2026-04-10 16:13:21 -0300

    ajusta charge spec

--- src/modules/billing/charges/charges.service.spec.ts
+++ src/modules/billing/charges/charges.service.spec.ts
@@ -3,6 +3,10 @@ import { ChargesService } from './charges.service';
 import { ChargesRepository } from './charges.repository';
 import { ChargesEngine } from './charges.engine';
 import { ChargeGatewayService } from './charge-gateway.service';
+import { FinancialEventsService } from 'src/modules/Finance/core/financial-events/financial-events.service';
+import { ChargesFactory } from './factorys/charges.factory';
+import MySQLDatabase from 'src/modules/Config/Database/MySQLDatabase';
+import { ChargeEventsService } from './events/charge-events.service';
 
 describe('ChargesService', () => {
   let service: ChargesService;
@@ -13,15 +17,48 @@ describe('ChargesService', () => {
         ChargesService,
         {
           provide: ChargesRepository,
-          useValue: { loadExpense: jest.fn(), save: jest.fn() },
+          useValue: {
+            loadExpense: jest.fn(),
+            save: jest.fn(),
+          },
         },
         {
           provide: ChargesEngine,
-          useValue: { process: jest.fn() },
+          useValue: {
+            process: jest.fn(),
+          },
         },
         {
           provide: ChargeGatewayService,
-          useValue: { sendCharge: jest.fn() },
+          useValue: {
+            sendCharge: jest.fn(),
+          },
+        },
+        {
+          provide: ChargesFactory,
+          useValue: {
+            create: jest.fn(),
+          },
+        },
+        {
+          provide: MySQLDatabase,
+          useValue: {
+            transaction: jest.fn(),
+            commit: jest.fn(),
+            rollback: jest.fn(),
+          },
+        },
+        {
+          provide: ChargeEventsService,
+          useValue: {
+            register: jest.fn(),
+          },
+        },
+        {
+          provide: FinancialEventsService,
+          useValue: {
+            register: jest.fn(),
+          },
         },
       ],
     }).compile();
